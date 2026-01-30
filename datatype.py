062028d102626cee6bd659856f4a1f31e1714525
Saltanat <saltanatadilbekova387@gmail.com>
2026-01-30 21:13:43 +0500

    Add Practice1 Python basics examples

---
+++ datatype.py
@@ -0,0 +1,34 @@
+x = 5
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = frozenset({"apple", "banana", "cherry"})
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = str("Hello World")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = float(20.5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
---
+++ stringg.py
@@ -0,0 +1,43 @@
+print("Hello")
+print('Hello')
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+for x in "banana":
+  print(x)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+b = "Hello, World!"
+print(b[2:5])
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
+
